e8177adc8f74b80665608a39355c92a847f3fc58
Nimufu <angelonmyknees@gmail.com>
2026-05-18 23:23:21 -0300

    Spinning animation for Collectable to play randomly and rarely & Changed Pemming layer to Friendly and Goal layer to Obstacle to allow Pemming to step past level borders (and not require making some kind of sprite for Goal)

--- Assets/Scripts/Interactables/Collectable.cs
+++ Assets/Scripts/Interactables/Collectable.cs
@@ -8,7 +8,24 @@ public class Collectable : MonoBehaviour
     [SerializeField] int _value;
     [SerializeField] Animator _animator;
 
-    static readonly int APPEAR_HASH = Animator.StringToHash("Appear");
+    float _timer;
+    bool _hasAppeared;
+
+    static readonly int APPEAR_HASH = Animator.StringToHash("Appear"), SPIN_HASH = Animator.StringToHash("Spin");
+
+    void Update()
+    {
+        if(!_hasAppeared) { return; }
+
+        _timer += Time.deltaTime;
+
+        if(_timer >= 0)
+        {
+            _timer = UnityEngine.Random.Range(-600f, -120f);
+            _animator.SetTrigger(SPIN_HASH);
+        }
+    }
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -22,5 +39,7 @@ public class Collectable : MonoBehaviour
     public void SetAppearTrigger()
     {
         _animator.SetTrigger(APPEAR_HASH);
+        _timer = UnityEngine.Random.Range(-600f, -30f);
+        _hasAppeared = true;
     }
 }
