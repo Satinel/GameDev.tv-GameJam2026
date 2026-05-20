63ded2341924fce631b54a716bab39505383bbe1
Nimufu <angelonmyknees@gmail.com>
2026-05-20 11:40:37 -0300

    Functional typing effect in StoryTeller

--- Assets/Scripts/StoryTeller.cs
+++ Assets/Scripts/StoryTeller.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using Ink.Runtime;
 using System.Collections.Generic;
+using System.Collections;
 
 public class StoryTeller : MonoBehaviour
 {
@@ -16,12 +17,19 @@ public class StoryTeller : MonoBehaviour
     [SerializeField] TextMeshProUGUI _text, _leftNameText, _rightNameText;
     [SerializeField] GameObject _leftNamePlate, _rightNamePlate;
     [SerializeField] TextAsset[] _dialogues;
+    [SerializeField] float _textDelay = 0.25f;
+    [SerializeField] char _linebreakSymbol = '~', _pauseSymbol = '^';
+    WaitForSecondsRealtime _textDelayWait = new(0.05f), _textDelayDecaWait = new(0.5f);
     Story _story;
+    string _fullLine;
     int _dialogueIndex;
+    Coroutine _typingRoutine;
 
     [SerializeField] CharacterSO _emptyChar, _pemmingChar, _eelChar, _fishChar, _tigeyChar, _redChar;
+    CharacterSO _currentCharacter;
+    bool _isLeftAligned;
+    int _spriteIndex = 0;
 
-    const string _noContentString = "NO CONTENT FOUND!";
     const string CHARACTER_TAG = "character", LAYOUT_TAG = "layout", OPPOSITE_TAG = "opposite";
     const string PEMMING_NAME = "Pemming", EEL_NAME = "Eel", FISH_NAME = "Fish", TIGEY_NAME = "Tigey", RED_NAME = "Red";//, MOLE_NAME = "something..."
 
@@ -41,7 +49,9 @@ public class StoryTeller : MonoBehaviour
 
     void Start()
     {
-        _text.text = _noContentString;
+        _textDelayWait = new(_textDelay);
+        _textDelayDecaWait = new(_textDelay * 10);
+        _text.text = string.Empty;
     }
 
     void OnUnpausedWithStoryActive()
@@ -94,17 +104,87 @@ public class StoryTeller : MonoBehaviour
 
     public void ContinueStory()
     {
+        if(_typingRoutine != null)
+        {
+            StopCoroutine(_typingRoutine);
+            _typingRoutine = null;
+            SkipTyping();
+            return;
+        }
+
         if(_story.canContinue)
         {
-            _text.text = _story.Continue();
+            _typingRoutine = StartCoroutine(TextDisplayRoutine(_story.Continue()));
+            // _text.text = _story.Continue();
             HandleTags();
         }
         else
         {
             _canvas.enabled = false;
             OnStoryCanvasClosed?.Invoke();
-            _text.text = _noContentString;
+            _text.text = string.Empty;
             return;
+        }
+    }
+
+    IEnumerator TextDisplayRoutine(string story)
+    {
+        _text.text = string.Empty;
+        _spriteIndex = 0;
+        _fullLine = story;
+        foreach(char letter in _fullLine.ToCharArray())
+        {
+            if(letter == _linebreakSymbol)
+            {
+                _text.text += "\n";
+            }
+            else if(letter == _pauseSymbol)
+            {
+                yield return _textDelayDecaWait;
+            }
+            else
+            {
+                HandleCharacterTalking();
+                _text.text += letter;
+                yield return _textDelayWait;
+            }
+        }
+
+        _typingRoutine = null;
+    }
+
+    void HandleCharacterTalking()
+    {
+        if(_isLeftAligned)
+        {
+            _spriteIndex = _currentCharacter.LeftSprites.Length > _spriteIndex ? _spriteIndex++ : 0;
+            _leftCharacter.sprite = _currentCharacter.LeftSprites[_spriteIndex];
+        }
+        else
+        {
+            _spriteIndex = _currentCharacter.RightSprites.Length > _spriteIndex ? _spriteIndex++ : 0;
+            _rightCharacter.sprite = _currentCharacter.RightSprites[_spriteIndex];
+        }
+    }
+
+    void SkipTyping()
+    {
+        _text.text = string.Empty;
+
+        foreach(char letter in _fullLine.ToCharArray())
+        {
+            if(letter == _linebreakSymbol)
+            {
+                _text.text += "\n";
+            }
+            else if(letter == _pauseSymbol)
+            {
+                continue;
+            }
+            else
+            {
+                _text.text += letter;
+            }
         }
     }
 
@@ -112,8 +192,8 @@ public class StoryTeller : MonoBehaviour
     {
         List<string> currentTags = _story.currentTags;
 
-        bool isLeftAligned = true;
-        CharacterSO character = _emptyChar;
+        _isLeftAligned = true;
+        _currentCharacter = _emptyChar;
         bool oppositeSpeaker = false;
 
         foreach(string tag in currentTags)
@@ -125,12 +205,12 @@ public class StoryTeller : MonoBehaviour
 
             if(tagKey == LAYOUT_TAG)
             {
-                isLeftAligned = tagValue == "left";
+                _isLeftAligned = tagValue == "left";
             }
 
             if(tagKey == CHARACTER_TAG)
             {
-                character = tagValue switch
+                _currentCharacter = tagValue switch
                 {
                     PEMMING_NAME => _pemmingChar,
                     EEL_NAME => _eelChar,
@@ -147,34 +227,32 @@ public class StoryTeller : MonoBehaviour
             }
         }
 
-        SetNamePlate(isLeftAligned, character, oppositeSpeaker);
+        SetNamePlate(oppositeSpeaker);
     }
 
-    void SetNamePlate(bool isLeftAligned, CharacterSO character, bool oppositeSpeaker)
+    void SetNamePlate(bool oppositeSpeaker)
     {
-        if(isLeftAligned)
+        if(_isLeftAligned)
         {
             _rightNamePlate.SetActive(false);
             _rightCharacter.enabled = oppositeSpeaker;
             _rightCharacter.color = Color.gray3;
-            _leftNameText.text = character.CharacterName;
+            _leftNameText.text = _currentCharacter.CharacterName;
             _leftNamePlate.SetActive(true);
             _leftCharacter.color = Color.white;
             _leftCharacter.enabled = true;
-            _leftCharacter.sprite = character.LeftSprites[0];
-            // _leftCharacterAlt.sprite = sprite1;
+            _leftCharacter.sprite = _currentCharacter.LeftSprites[0];
         }
         else
         {
             _leftNamePlate.SetActive(false);
             _leftCharacter.enabled = oppositeSpeaker;
             _leftCharacter.color = Color.gray3;
-            _rightNameText.text = character.CharacterName;
+            _rightNameText.text = _currentCharacter.CharacterName;
             _rightNamePlate.SetActive(true);
             _rightCharacter.color = Color.white;
             _rightCharacter.enabled = true;
-            _rightCharacter.sprite = character.RightSprites[0];
-            // _rightCharacterAlt.sprite = sprite1;
+            _rightCharacter.sprite = _currentCharacter.RightSprites[0];
         }
     }
 }
