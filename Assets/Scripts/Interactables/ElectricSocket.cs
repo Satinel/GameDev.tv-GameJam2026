a86f345cb4e41ca00c1ff87b397bd8ade87f5976
Nimufu <angelonmyknees@gmail.com>
2026-05-20 16:07:11 -0300

    Animations for Electric Socket & Scenes 1 and 2 are genuinely probably done now (but they aren't great)

--- Assets/Scripts/Interactables/ElectricSocket.cs
+++ Assets/Scripts/Interactables/ElectricSocket.cs
@@ -8,11 +8,38 @@ public interface IElectrifiable
 
 public class ElectricSocket : MonoBehaviour
 {
+    [SerializeField] Animator _animator;
+
+    static readonly int FLICKER_HASH = Animator.StringToHash("Flicker"), ZAP_HASH = Animator.StringToHash("Zap");
+
+    float _timer;
+    bool _isZapping;
+
+    void Start()
+    {
+        _timer = Random.Range(7.5f, 45f);
+    }
+
+    void Update()
+    {
+        if(_isZapping) { return; }
+
+        _timer -= Time.deltaTime;
+
+        if(_timer <= 0)
+        {
+            _timer = Random.Range(7.5f, 45f);
+            _animator.SetTrigger(FLICKER_HASH);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent(out IElectrifiable electrifiable))
         {
             electrifiable.Electrify();
+            _isZapping = true;
+            _animator.SetBool(ZAP_HASH, _isZapping);
         }
     }
 
@@ -21,6 +48,8 @@ public class ElectricSocket : MonoBehaviour
         if(collision.TryGetComponent(out IElectrifiable electrifiable))
         {
             electrifiable.Delectrify();
+            _isZapping = false;
+            _animator.SetBool(ZAP_HASH, _isZapping);
         }
     }
 }
